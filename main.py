d0b20dc1dac1134ba2d986c254d944454b95ef9d
tlashany-gems <robinjoshua65432@gmail.com>
2026-02-21 16:09:44 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ from pytgcalls import PyTgCalls, idle
 from pytgcalls.types import MediaStream
 
 # ─── إعدادات ──────────────────────────────────────────────────────────────────
-API_ID         = int(os.environ.get("API_ID", "0"))
-API_HASH       = os.environ.get("API_HASH", "")
-BOT_TOKEN      = os.environ.get("BOT_TOKEN", "")
-STRING_SESSION = os.environ.get("STRING_SESSION", "")
+API_ID         = 21173110
+API_HASH       = "71db0c8aae15effc04dcfc636e68c349"
+BOT_TOKEN      = "5715894811:AAEn1rgGrt98NbqlkcGPyz0As4mLv_I65qw"
+STRING_SESSION = "BAGOXzsAY2IZkkdbaL7f80ukOhwdLcEzj0r7QLYvq2c7Aq6cWDadPvHWTGmh1UTEgY4PcuU7eI8Ewoa5mvLaYfhKmoNoWHz0kvk4G1vUnVs-WT_x5JDONtCNX9N8Op1bl8Q-VIXlzs40mb8zJ7w5my08ab3bazAqfkEesSjLUiGatkh3FRTKFBpSyxOZMNoQspTniv9Ou86UnOXlLAefamYo-7M4IKIkZInZd_tvFpzM8PYr7er6hapjgGGIGmH6IrW2uco9H44IATu5MeHliIgO4nmD3nvv6VN2ra8-fkqVZfxTZtSUFNOOjmVOORDhbqfxRmhaPAU4qZKuBOozwqKZAAAAAHYtkk7AA"
 
 # ─── تهيئة الـ Clients ────────────────────────────────────────────────────────
 userbot = Client(
