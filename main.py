bda23c0e1964c7e693e7bfbf3082fe33817b22f5
tlashany-gems <robinjoshua65432@gmail.com>
2026-02-21 16:21:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from pytgcalls.types import MediaStream
 API_ID         = 21173110
 API_HASH       = "71db0c8aae15effc04dcfc636e68c349"
 BOT_TOKEN      = "5715894811:AAEn1rgGrt98NbqlkcGPyz0As4mLv_I65qw"
-STRING_SESSION = "BAGOXzsAY2IZkkdbaL7f80ukOhwdLcEzj0r7QLYvq2c7Aq6cWDadPvHWTGmh1UTEgY4PcuU7eI8Ewoa5mvLaYfhKmoNoWHz0kvk4G1vUnVs-WT_x5JDONtCNX9N8Op1bl8Q-VIXlzs40mb8zJ7w5my08ab3bazAqfkEesSjLUiGatkh3FRTKFBpSyxOZMNoQspTniv9Ou86UnOXlLAefamYo-7M4IKIkZInZd_tvFpzM8PYr7er6hapjgGGIGmH6IrW2uco9H44IATu5MeHliIgO4nmD3nvv6VN2ra8-fkqVZfxTZtSUFNOOjmVOORDhbqfxRmhaPAU4qZKuBOozwqKZAAAAAHYtkk7AA"
+STRING_SESSION = "BAGOXzsAY2IZkkdbaL7f80ukOhwdLcEzj0r7QLYvq2c7Aq6cWDadPvHWTGmh1UTEgY4PcuU7eI8Ewoa5mvLaYfhKmoNoWHz0kvk4G1vUnVs-WT_x5JDONtCNX9N8Op1bl8Q-VIXlzs40mb8zJ7w5my08ab3bazAqfkEesSjLUiGatkh3FRTKFBpSyxOZMNoQspTniv9Ou86UnOXlLAefamYo-7M4IKIkZInZd_tvFpzM8PYr7er6hapjgGGIGmH6IrW2uco9H44IATu5MeHliIgO4nmD3nvv6VN2ra8-fkqVZfxTZtSUFNOOjmVOORDhbqfxRmhaPAU4qZKuBOozwqKDomPKZAAAAAHYtkk7AA"
 
 # ─── تهيئة الـ Clients ────────────────────────────────────────────────────────
 userbot = Client(
